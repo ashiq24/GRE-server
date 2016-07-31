1cc7bda818100f392fc8f1772389a1153febe716
ashiq24 <ashiqbuet14@gmail.com>
2016-07-31 20:58:56 +0600

    final one

--- src/Loadingserver/Controller.java
+++ src/Loadingserver/Controller.java
@@ -2,14 +2,17 @@ package Loadingserver;
 
 import javafx.event.ActionEvent;
 import javafx.scene.*;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
     Server s;
+   public  Label on;
     public void startaction(ActionEvent e) throws IOException, InterruptedException {
         s=new Server(9000);
+        on.setText("SERVER IS ON");
     }
     public void closeaction(ActionEvent e) throws IOException {
         Stage homesateg= (Stage) ((javafx.scene.Node) e.getSource()).getScene().getWindow();
--- src/newlogin/Controller.java
+++ src/newlogin/Controller.java
@@ -1,13 +1,16 @@
 package newlogin;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 public class Controller {
     server s;
+    public Label on;
     public void startaction(ActionEvent e) throws IOException, InterruptedException {
         s=new server(8000);
+        on.setText("SERVER IN ON");
     }
     public void closeaction(ActionEvent e) throws IOException {
         Stage homesateg= (Stage) ((javafx.scene.Node) e.getSource()).getScene().getWindow();
